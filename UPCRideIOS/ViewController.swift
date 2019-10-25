e19911a6ea83c98d7db12410ff12e6e3a84c59fa
Sebastian <cardiacchino@gmail.com>
2019-10-25 07:39:46 -0500

    UPCRideIOS

---
+++ UPCRideIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UPCRideIOS
+//
+//  Created by Developer on 10/25/19.
+//  Copyright © 2019 UPCRide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
